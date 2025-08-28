df8dfa4c62fd13d083f12cdeecc6b1395024be91
Jacob <jschepis02@gmail.com>
2025-08-28 16:22:18 +1000

    Initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const navbar = document.getElementById('navbar')
+
+function openSidebar() {
+    navbar.classList.add('show')
+}
+
+function closeSidebar() {
+    navbar.classList.remove('show')
+}
